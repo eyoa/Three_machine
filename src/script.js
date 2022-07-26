31c250531dbfb4582d7bcdca85e8da10781042d0
Evelyn Yoa <Evelyn.Yoa@esentire.com>
2022-07-25 21:23:56 -0400

    add mouse click

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
 
 /**
  * Debug
@@ -29,8 +29,20 @@ debugObject.createBox = () => {
             z: (Math.random() - 0.5) * 3,
         })
 }
+debugObject.reset = () => {
+    for (const object of objectsToUpdate){
+        // Remove Body
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+        // Remove Mesh
+        scene.remove(object.mesh)
+    }
+    objectsToUpdate = []
+}
 gui.add(debugObject, 'createSphere')
 gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
+
 
 /**
  * Base
@@ -40,6 +52,23 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Sounds
+ */
+
+const hitSound = new Audio('/sounds/hit.mp3')
+
+const playHitSound = (collision) => {
+    const impactStrength = collision.contact.getImpactVelocityAlongNormal()
+    if (impactStrength > 1.5 ){ 
+        hitSound.volume = Math.min(Math.max(impactStrength, 0), 1) * 0.5
+        hitSound.currentTime = 0
+        hitSound.play()
+    }
+}
+
+
 
 /**
  * Textures
@@ -148,6 +177,34 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Cursor
+ */
+const mouse = new THREE.Vector2()
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.clientX /sizes.width * 2 - 1
+    mouse.y = - (event.clientY /sizes.height * 2) + 1
+})
+
+window.addEventListener('click', () => {
+    if (currentIntersect){
+        switch(currentIntersect.object){
+            case buttonObj:
+                createBox(
+                    Math.random(),
+                    Math.random(),
+                    Math.random(), 
+                    {
+                        x: (Math.random() - 0.5) * 3,
+                        y: 3,
+                        z: (Math.random() - 0.5) * 3,
+                    })
+                break
+        }
+
+    }
+})
+
+/**
  * Camera
  */
 // Base camera
@@ -174,7 +231,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Utils
  */
-const objectsToUpdate = []
+let objectsToUpdate = []
 
 // sphere
 const sphereGeometry =  new THREE.SphereBufferGeometry(1, 20, 20)
@@ -200,6 +257,7 @@ const createSphere = (radius, position) => {
         shape,
     })
     body.position.copy(position)
+    body.addEventListener('collide', playHitSound)
     world.addBody(body)
 
     // save in objects to update
@@ -234,6 +292,7 @@ const createBox = (width, height, depth, position) => {
         shape,
     })
     body.position.copy(position)
+    body.addEventListener('collide', playHitSound)
     world.addBody(body)
 
     // save in objects to update
@@ -246,9 +305,41 @@ const createBox = (width, height, depth, position) => {
 
 
 // initial scene
-createSphere(0.5, {x: 0, y: 3, z: 0})
+createSphere(0.5, {x: 0, y: 3, z: 2})
+
+/**
+ * Button
+ */
+// mesh
+const buttonObj = new THREE.Mesh(boxGeometry, boxMaterial)
+
+buttonObj.scale.set(1, 0.2, 1)
+buttonObj.castShadow = true
+buttonObj.position.copy({x: 2, y: 0, z: 0})
+scene.add(buttonObj)
+
+// physics body
+const shape = new CANNON.Box(new CANNON.Vec3(0.5, 0.2 * 0.5, 0.5))
+const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(2,0,0),
+    shape,
+})
+body.position.copy({x: 2, y: 0, z: 0})
+world.addBody(body)
 
 
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10,0,0)
+// rayDirection.normalize()
+// raycaster.set(rayOrigin, rayDirection)
+// const intersect = raycaster.intersectObject(buttonObj)
+// console.log(intersect)
 
 
 /**
@@ -256,11 +347,34 @@ createSphere(0.5, {x: 0, y: 3, z: 0})
  */
 const clock = new THREE.Clock()
 let oldElapsedTime = 0
+
+let currentIntersect = null
+
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
     const deltaTime = elapsedTime - oldElapsedTime
     oldElapsedTime = elapsedTime
+
+    // cast a ray
+    raycaster.setFromCamera(mouse, camera)
+    
+    const objectsToTest = [buttonObj]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+
+    if (intersects.length){
+        if (currentIntersect === null){
+            console.log('mouse Enter')
+        }
+        currentIntersect = intersects[0]
+    }
+    else {
+        if (currentIntersect){
+            console.log('mouse leave')
+        }
+        currentIntersect = null
+    }
+
 
     // Update physics world
     world.step(1/60, deltaTime, 3)
